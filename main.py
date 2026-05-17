4cab1912fd08dbd463b172fd840b736d12fef32d
ErnestoGtz <ernesto.gutierrez9298@alumnos.udg.mx>
2026-05-17 13:40:01 -0600

    feat-> Clases para guardar los casilleros de huevos y los huevos

--- main.py
+++ main.py
@@ -10,6 +10,35 @@
 # A partir de ahí cada huevo tendría su método de fuzzificación donde cada atributo iría a la función
 # de membresía triangular o trapezoidal.
 
+class Casillero:
+    def __init__(self, matriz_huevos,cantidad_plumas):
+        self.matriz_huevos = matriz_huevos #idealmente, que siempre sean 60 (es un arraylist de
+                                            #objetos tipo Huevo)
+        self.cantidad_plumas = cantidad_plumas #cantidad de plumas dentro de la caja (en unidades)
+
+class Huevo:
+    def __init__(self,area_mancha,grosor_fisura,
+                 presencia_derrames,
+                 interior_limpio,ancho,alto,profundidad,densidad_relativa,area_moteado,
+                 distancia_camara_interna,matriz_colores,matriz_alturas):
+        self.area_mancha = area_mancha #el área total de las manchas superficiales (en milímetros)
+        self.grosor_fisura = grosor_fisura #el grosor de la fisura externa más ancha (en micrómetros)
+        self.presencia_derrames = presencia_derrames #presencia de derrames (booleano)
+        self.interior_limpio = interior_limpio #interior sin microbios ni inclusiones (booleano)
+        self.ancho = ancho #ancho del huevo (en cm)
+        self.alto = alto #alto del huevo (en cm)
+        self.profundidad = profundidad #profundidad del huevo (en cm)
+        self.densidad_relativa = densidad_relativa #densidad relatva (un flotante adimensional entre 1 y 1.15, aprox.)
+        self.area_moteado = area_moteado #área de la superficie que está moteada (un flotante, en cm²)
+        self.distancia_camara_interna = distancia_camara_interna #distancia de cámara interna (en milímetros)
+        self.matriz_colores = matriz_colores #dividiendo la superficie en pixeles de colores, 
+                                            #los colores de cada pixel en RGB
+        self.matriz_alturas = matriz_alturas #dividiendo la superficie en irregularidades, 
+                                            #las coordenadas en el eje y
+        
+
+
+
 # *** Base de conocimiento *** #
 # Después entra en una de las clasificaciones de cada característica
 # ej. 0.3 de pequeño, 0. 7 de mediano, 0.5 de grande. (Aquí podríamos hacer varias cosas quedarnos con el más significativo,
